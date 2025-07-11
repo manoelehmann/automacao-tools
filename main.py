958adac70147200f778e89cd6a2b8e04aa750c92
Manoel <manoelnetoteixeirarodrigues@gmail.com>
2025-07-11 13:41:14 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+navegador = webdriver.Chrome()
+navegador.get('https://www.youtube.com/')
+navegador.maximize_window()
+espera = WebDriverWait(navegador, 20)
